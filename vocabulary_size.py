a9a50559bba0f567a4d4ef94d94812f7dc2bf4f7
Rahul H R <hunasehallir@wisc.edu>
2021-10-10 00:11:21 -0500

    vocab count code

---
+++ vocabulary_size.py
@@ -0,0 +1,15 @@
+words = []
+with open("tweets.txt", "r") as filestream:
+  for line in filestream:
+    currentline = line.split(",")
+    currentline.pop(0)
+    for i in currentline:
+      j = i.replace("\n", '')
+      words.append(j)
+
+print(words)
+uniq_words = set(words)
+uniq_count = len(uniq_words)
+print(uniq_words)
+print(uniq_count)
+
